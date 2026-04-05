8ef3b0f7deb65bae64f307d24c822043d5f17b32
park-jinh <jinh.drive@gmail.com>
2026-04-05 17:46:49 +0900

    docs: add server list to OpenApiConfig

--- src/main/java/com/jinh/boilerplate/global/config/OpenApiConfig.java
+++ src/main/java/com/jinh/boilerplate/global/config/OpenApiConfig.java
@@ -2,9 +2,12 @@ package com.jinh.boilerplate.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 /**
  * Swagger (Springdoc OpenAPI) 설정 클래스
@@ -19,6 +22,12 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Spring Boot 4.0 API Boilerplate")
                         .description("Java 21 및 Spring Boot 4.0 기반의 API 표준 보일러플레이트 명세서입니다.")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("개발용 서버")
+
+                ));
     }
 }
